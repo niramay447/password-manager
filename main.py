a2647122f70b3744e50063144591f9a2ddadd82c
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-01-25 11:35:30 +0530

    search funtionality

--- main.py
+++ main.py
@@ -56,6 +56,16 @@ def save():
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
+#----------------------FIND PASSWORD-------------------------------#
+def find_password():
+    website = website_entry.get()
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        print(data)
+        if website in data:
+            email= data[website]["email"]
+        password = data[website]["password"]
+        messagebox.showinfo(title=website, message=f"Email:{email}\nPassword:{password}")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -75,8 +85,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=40)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -85,8 +95,12 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width =13, command = find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+
 window.mainloop()
